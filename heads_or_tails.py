1e09644a85bb406eccf79d1e8c0a91557a3fa2ac
MuneebSaab <150551096+MuneebSaab@users.noreply.github.com>
2024-04-25 20:06:45 +0400

    heads_or_tails.py
    Heads or Tails Python Program

---
+++ heads_or_tails.py
@@ -0,0 +1,8 @@
+# In this Python Program, I have setup online or virtual toss generating Random Side whenever user asks.
+
+import random
+random_side = random.randint(0,1)
+if random_side ==1:
+  print("Heads")
+else:
+  print("Tails")
